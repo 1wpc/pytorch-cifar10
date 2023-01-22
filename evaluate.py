75be372b3b853321199f7d77d4928c61bf343905
wang <93064853+1wpc@users.noreply.github.com>
2023-01-22 12:20:18 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -3,14 +3,9 @@ import numpy as np
 import torchvision.transforms as transforms
 import torchvision
 import torch
-from test_net import Net
+from test_net import Net,testloader,classes,batch_size
 
 net=torch.load('modle.pth')
-#读取测试集
-transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
-test_set=torchvision.datasets.CIFAR10(root='./data',train=False,download=True,transform=transform)
-testloader=torch.utils.data.DataLoader(test_set,batch_size=4,shuffle=False)
-classes=('飞机','轿车','鸟','猫','鹿','狗','蛙','马','船','货车')
 
 def imshow(img):
     img=img/2+0.5  #逆归一化
@@ -30,9 +25,14 @@ def evaluate():
             correct+=(predict==labels).sum().item()
     return 100*correct/total
 
-dataiter=iter(testloader)
-imgs,labels=next(dataiter)
-imshow(torchvision.utils.make_grid(imgs))
-print(''.join('%5s'%classes[labels[j]] for j in range(4)))
+def test():
+    dataiter=iter(testloader)
+    imgs,labels=next(dataiter)
+    print(''.join('%5s'%classes[labels[j]] for j in range(batch_size)))
+    outs=net(imgs)
+    _,preds=torch.max(outs.data,1)
+    print(''.join('%5s'%classes[preds[j]] for j in range(batch_size)))
+    imshow(torchvision.utils.make_grid(imgs))
 
 print(evaluate())
+test()
